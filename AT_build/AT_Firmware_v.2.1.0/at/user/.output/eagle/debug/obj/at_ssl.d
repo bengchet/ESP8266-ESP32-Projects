493c67ee1735adc5cf38d522b73ddf46796ba86f
bengchet <bengchet@cytron.com.my>
2017-10-30 10:27:11 +0800

    updated version to 2.1.0

---
+++ AT_build/AT_Firmware_v.2.1.0/at/user/.output/eagle/debug/obj/at_ssl.d
@@ -0,0 +1,21 @@
+.output/eagle/debug/obj/at_ssl.o .output/eagle/debug/obj/at_ssl.d : at_ssl.c ../../include/user_interface.h ../../include/os_type.h \
+ ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/ip_addr.h ../../include/queue.h \
+ ../include/user_config.h ../../include/spi_flash.h ../../include/gpio.h \
+ ../../include/espconn.h ../../include/at_custom.h ../../include/osapi.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /opt/Espressif/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h
---
+++ AT_build/AT_Firmware_v.2.1.0/at/user/.output/eagle/debug/obj/user_main.d
@@ -0,0 +1,22 @@
+.output/eagle/debug/obj/user_main.o .output/eagle/debug/obj/user_main.d : user_main.c ../../include/osapi.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/string.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/newlib.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/config.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/features.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/reent.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/_ansi.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/_types.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_types.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/machine/_default_types.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/lock.h \
+ /opt/Espressif/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.5/include/stddef.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/cdefs.h \
+ /opt/Espressif/xtensa-lx106-elf/xtensa-lx106-elf/include/sys/string.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../include/user_config.h \
+ ../../include/at_custom.h ../../include/user_interface.h \
+ ../../include/ip_addr.h ../../include/queue.h ../../include/spi_flash.h \
+ ../../include/gpio.h
